135db492206951abaca774935dd7e5f86964e124
George Imhandegbelo <46719329+Imhandegbelo@users.noreply.github.com>
2025-10-15 14:19:04 +0100

    setup base url for deployment\

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   // preview: {
   //   origin: 'http://127.0.0.1:5173',
   //   port: 5173,
