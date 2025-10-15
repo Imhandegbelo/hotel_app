15b0bfe9b39f4e908474e61d5a797a97ea606ab2
George Imhandegbelo <46719329+Imhandegbelo@users.noreply.github.com>
2025-10-15 14:43:51 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/hotel_app/",
   // preview: {
   //   origin: 'http://127.0.0.1:5173',
   //   port: 5173,
