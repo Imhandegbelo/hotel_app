9361895fa1fbc4979268e8e6dcd2a74a9a40f159
Imhandegbelo <abramgeo5@gmail.com>
2025-10-16 09:10:02 +0100

    update: CI/CD

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/hotel_app/",
+  build: {
+    outDir: "dist",
+  },
   // preview: {
   //   origin: 'http://127.0.0.1:5173',
   //   port: 5173,
   // },
-})
+});
